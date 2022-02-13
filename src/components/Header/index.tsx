20b061f84043150f34a8b936831be6057a087d88
Pavel Harmaza <pavelharmaza@gmail.com>
2022-02-13 23:58:19 +0300

    refactor: add routes object

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,50 +1,51 @@
 import React, { FC, useState } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-// import DropdownMenu from "../DropdownMenu";
-// const [isVisible, setIsVisible] = useState(false);
-
+import DropdownMenu from "../DropdownMenu";
+import { ROUTES } from "../../routes";
 const Header: FC = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
   return (
     <header>
       <h1 className="header-logo">Logo</h1>
       <nav>
         <ul>
           <li className="nav-link">
-            <Link to="/" className="route-link">
+            <Link to={ROUTES.INITIAL} className="route-link">
               Home
             </Link>
           </li>
           <li className="nav-link">
-            <Link to="/about" className="route-link">
+            <Link to={ROUTES.ABOUT} className="route-link">
               About
             </Link>
           </li>
           <li
             className="nav-link"
-            // onMouseEnter={() => setIsVisible(true)}
-            // onMouseLeave={() => setIsVisible(false)}
+            onMouseEnter={() => setIsVisible(true)}
+            onMouseLeave={() => setIsVisible(false)}
           >
-            <Link to="/products" className="route-link">
+            <Link to={ROUTES.PRODUCTS} className="route-link">
               Products
             </Link>
+            {isVisible && <DropdownMenu />}
           </li>
         </ul>
       </nav>
       <h1 className="header-cart">
-        <Link to="/cart" className="route-link">
+        <Link to={ROUTES.CART} className="route-link">
           Cart
         </Link>
       </h1>
       <div className="header-sign">
         <button className="sign-in">
-          <Link to="/signin" className="route-link">
+          <Link to={ROUTES.SIGNIN} className="route-link">
             Sign In
           </Link>
         </button>
         <button className="sign-up">
-          {" "}
-          <Link to="/signup" className="route-link">
+          <Link to={ROUTES.SIGNUP} className="route-link">
             Sign Up
           </Link>
         </button>
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  INITIAL: "/",
+  HOME: "/home",
+  ABOUT: "/about",
+  PRODUCTS: "/products",
+  CART: "/cart",
+  SIGNIN: "/signin",
+  SIGNUP: "/signup",
+};
