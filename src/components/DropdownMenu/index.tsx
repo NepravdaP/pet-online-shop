6e16b983a8f78b9cddb653a0126aee8718fe4182
Pavel Harmaza <pavelharmaza@gmail.com>
2022-02-14 00:46:51 +0300

    feat: add dropdown menu

--- src/components/DropdownMenu/index.tsx
+++ src/components/DropdownMenu/index.tsx
@@ -1,13 +1,21 @@
 import React, { FC } from "react";
+import "./style.css";
 import { Link } from "react-router-dom";
+
 const DropdownMenu: FC = () => {
   return (
-    <div className="drop-down-menu">
+    <div className="dropdown-menu">
       <div className="drop-triangle"></div>
       <div className="drop-block">
-        <Link to="/products/pc">PC</Link>
-        <Link to="/products/ps">PS</Link>
-        <Link to="/products/xbox">Xbox</Link>
+        <Link to="/products/pc" className="drop-link">
+          PC
+        </Link>
+        <Link to="/products/ps" className="drop-link">
+          PS
+        </Link>
+        <Link to="/products/xbox" className="drop-link">
+          Xbox
+        </Link>
       </div>
     </div>
   );
